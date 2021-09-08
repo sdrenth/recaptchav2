d190f7983a509ddd2f468d5fd7205a90face1fdd
Sander Drenth <sanderd.drenth@gmail.com>
2021-09-08 13:07:50 +0200

    Added Dutch translations

---
+++ core/components/recaptchav2/lexicon/nl/default.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Dutch Lexicon Entries for recaptchav2
+ *
+ * @package recaptchav2
+ * @subpackage lexicon
+ *
+ */
+$_lang['recaptchav2.technical_error_message'] = 'Sorry, er is een fout opgetreden bij het verzenden van uw formulier. Gebruik in plaats daarvan een van de contactpersonen op deze pagina.';
+$_lang['recaptchav2.recaptcha_error_message'] = 'Selecteer het selectievakje in de ReCaptcha-afbeelding.';
+$_lang['recaptchav2.recaptchav3_error_message'] = 'Fout bij het verifiëren van reCAPTCHA, probeer het opnieuw.';
